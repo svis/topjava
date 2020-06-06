def5086a3c3462360a935ac0fa939d88e7ee24eb
Sergey Visker <sergey.visker@gmail.com>
2020-06-07 01:25:52 +0300

    HW01: Add MealServlet

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,16 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static org.slf4j.LoggerFactory.getLogger;
-
-public class UserServlet extends HttpServlet {
-    private static final Logger log = getLogger(UserServlet.class);
+public class MealServlet extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(MealServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        log.debug("forward to users");
-        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        log.info("getAll");
+        request.setAttribute("meals", MealsUtil.getWithExceeded(MealsUtil.meals));
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
-
