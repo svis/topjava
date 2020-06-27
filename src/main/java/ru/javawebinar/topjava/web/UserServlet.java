b54a5bf73838e5bd5309bfdee5e32795215058b5
svis <sergey.visker@gmail.com>
2020-06-27 20:41:56 +0300

    L4_align with origin

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -22,9 +22,7 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         log.debug("forward to users");
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
 }
-
